01cb5f73da99819b2aad9804e41eca4e872903c3
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-03-25 14:41:52 -0400

    Updated from Brython Server: 3/25/2016 2:41:52 PM

--- distribution.py
+++ distribution.py
@@ -37,12 +37,14 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 userInput = input("Please enter a string of text (the bigger the better): ")
+userInput = userInput.lower()
 
 characters = {"a":"","b":"","c":"","d":"","e":"","f":"","g":"","h":"","i":"","j":"","k":"","l":"","m":"","n":"","o":"","p":"","q":"","r":"","s":"","t":"","u":"","v":"","w":"","x":"","y":"","z":""}
 inputList = []
 
-for s in inputList:
-    characters[s]+=s
+for s in userInput:
+    if s != " ":
+        characters[s]+=s
 
 for i in characters:
     print(i)
